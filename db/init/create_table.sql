a7e6790e6a5164bb74cb562dc6b11a4aa1a27842
remote-client <http.create.jp@gmail.com>
2020-08-26 17:29:48 +0900

    fix create_table.sql 不具合対応

--- db/init/create_table.sql
+++ db/init/create_table.sql
@@ -1,6 +1,6 @@
-drop table TEST_USER
+drop table if exists TEST_USER;
 
-CREATE TABLE TEST_USER (
+create table TEST_USER (
     user_id BIGINT PRIMARY KEY,
     user_password VARCHAR(20) NOT NULL
 );
